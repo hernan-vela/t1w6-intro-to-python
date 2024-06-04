d92e83007d3d9cc50fa318d3b68729b9b31b34f7
hernan-vela <hernan.velamar@gmail.com>
2024-06-04 21:03:40 +1000

    program with inputs

---
+++ area_of_square.py
@@ -0,0 +1,9 @@
+#get the length from the user
+length = float(input("Enter the length of the square: "))
+
+#print(length)
+#calculate and store the area in a variable
+area = length * length
+
+#print the calculated area to the terminal
+print(f"The calculated area for this square is {area}")
---
+++ create_email_form.py
@@ -0,0 +1,6 @@
+#Hernan Velasquez Coder Academy -> 15524@coderacademy.eud.au
+first_name = input("Please enter your first name: ")
+last_name = input("Please enter your last name: ")
+company_name = input("Please enter your company name (no blank spaces): ")
+email = f"{first_name}.{last_name}@{company_name}.com".lower()
+print(email)
